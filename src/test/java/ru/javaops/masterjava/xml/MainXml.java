48666e2c8f9e92029f4288188d4a4b7bb8a77690
Andrei Bogdanov <andrei_bogdanov@epam.com>
2018-07-21 15:59:39 +0300

    add html with users belonged to project

--- src/test/java/ru/javaops/masterjava/xml/MainXml.java
+++ src/test/java/ru/javaops/masterjava/xml/MainXml.java
@@ -1,6 +1,8 @@
 package ru.javaops.masterjava.xml;
 
 import com.google.common.io.Resources;
+import j2html.TagCreator;
+import j2html.tags.ContainerTag;
 import ru.javaops.masterjava.xml.schema.*;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
@@ -9,8 +11,13 @@ import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static j2html.TagCreator.*;
 
 public class MainXml {
     public static void main(String[] args) throws Exception {
@@ -18,9 +25,37 @@ public class MainXml {
         Scanner scanner = new Scanner(System.in);
         String projectName = scanner.next();
 
-        jaxbFindUsersInProject(projectName);
+        List<User> users = jaxbFindUsersInProject(projectName);
         System.out.println("______________");
         staxFindUsersInProject(projectName);
+        System.out.println("Convert to html");
+
+        ContainerTag table = table().with(
+                tr().with(
+                        th("full name"),
+                        th("email")
+                ),
+                each(users, user -> tr().with(
+                        td(user.getFullName()),
+                        td(user.getEmail())
+                ))
+        ).attr("border", "1")
+                .attr("cellpadding", "8")
+                .attr("cellspacing", "0");
+
+
+        String html = TagCreator.html().with(
+                head().with(
+                        title("hello from j2html")
+                ),
+                body().with(
+                        table
+                )
+        ).render();
+        try (Writer writer = Files.newBufferedWriter(Paths.get("out/users.html"))) {
+            writer.write(html);
+        }
+
     }
 
     private static void staxFindUsersInProject(String projectName) throws Exception {
@@ -66,7 +101,7 @@ public class MainXml {
         return user;
     }
 
-    private static void jaxbFindUsersInProject(String projectName) throws javax.xml.bind.JAXBException, IOException {
+    private static List<User> jaxbFindUsersInProject(String projectName) throws javax.xml.bind.JAXBException, IOException {
         JaxbParser jaxbParser = new JaxbParser(ObjectFactory.class);
         jaxbParser.setSchema(Schemas.ofClasspath("payload.xsd"));
         Payload payload = jaxbParser.unmarshal(
@@ -76,11 +111,12 @@ public class MainXml {
                 .filter(group -> ((Project) group.getProject()).getName().equals(projectName))
                 .collect(Collectors.toList());
 
-        List<User> collect = payload.getUsers().getUser().stream()
+        List<User> result = payload.getUsers().getUser().stream()
                 .filter(user -> !Collections.disjoint(groupsForChosenProject, user.getGroupRefs()))
                 .sorted(Comparator.comparing(User::getFullName))
                 .collect(Collectors.toList());
-        collect.forEach(user -> System.out.println(user.getFullName() + " " + user.getEmail()));
+        result.forEach(user -> System.out.println(user.getFullName() + " " + user.getEmail()));
+        return result;
     }
 
 }
